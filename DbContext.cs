f2aa3f3c5cedf48465e6672a0f14aa5bf3f99fd4
Tony Arntsen <tonyarntsen@tonys-air.lan>
2024-02-28 15:56:58 +0100

    Guesswork 2

--- DbContext.cs
+++ DbContext.cs
@@ -12,7 +12,8 @@ namespace geo
             if (!optionsBuilder.IsConfigured)
             {
                 // Specify the connection string for your SQL Server database
-                optionsBuilder.UseSqlServer("Server=(34.140.79.76);Database=messages;User Id=geochat-415222:europe-west1:geochatdb;Password=kakemot1;");
+                            string con = "User Id=<DB_USER>;Password=kakemot1;Server=34.140.79.76;Database=messages;";
+                optionsBuilder.UseSqlServer(con);
             }
         }
     }
